5deb97f35ef45f97d5db51c1a5b2ca4de033fa61
falldemba <fall.demba.info@gmail.com>
2020-02-14 14:30:21 +0000

    init app

---
+++ src/components/Users.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="container">
+    <h3>Users:</h3>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">City</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="user in users" v-bind:key="user.id">
+                <th scope="row">{{user.id}}</th>
+                <td>{{user.username}}</td>
+                <td>{{user.password}}</td>
+             </tr>
+          </tbody>
+     </table>
+ </div>
+</template>
+<script>
+    import axios from 'axios';
+    export default {
+        name: 'Users',
+        data() {
+            return {
+            users: null,
+            };
+        },
+        created: function() {
+            axios.get('http://localhost:3000/api/v1/users')
+            .then(res => {
+            this.users = res.data;
+            })
+        }
+    }
+</script>
+<style>
+h3 {
+    margin-bottom: 5%;
+}
+</style>
